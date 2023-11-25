8bb113170a612594cb3e6b1a45c24e6eb2f1d197
Denilson Silva <denilsonarruda@outlook.com>
2023-11-25 18:39:59 -0300

    Created user model

---
+++ models/User.model.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../services/database";
+
+const User = sequelize.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: {
+          msg: "Endereço de email não pode ser vazio.",
+        },
+        isEmail: {
+          msg: "Insira um endereço de email válido.",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Senha não pode ser vazia." },
+      },
+    },
+  },
+  { underscored: true }
+);
+
+export default User;
