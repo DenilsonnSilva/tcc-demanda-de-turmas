bb31ed418c701f132a8d93d1ebc02d54c286662c
Denilson Silva <denilsonarruda@outlook.com>
2023-11-29 20:06:28 -0300

    Created user authentication middleware

---
+++ src/middlewares/UserAuthentication.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const userAuthentication = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (authorizationHeader) {
+    const token = authorizationHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.MY_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ message: "Token inválido." });
+      }
+
+      req.userId = decoded.userId;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({ message: "Token não fornecido." });
+  }
+};
+
+export default userAuthentication;
