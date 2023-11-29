a8f32a4b589eabda60f36479067a82393f69b8c9
Denilson Silva <denilsonarruda@outlook.com>
2023-11-29 13:35:46 -0300

    Created teacher controller

---
+++ src/controllers/TeacherController.js
@@ -0,0 +1,68 @@
+import Teacher from "../models/teacher.model";
+
+const createTeacher = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const newTeacher = await Teacher.create({ name });
+
+    return res.status(200).json(newTeacher);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const readTeacher = async (req, res) => {
+  try {
+    const teachers = await Teacher.findAll();
+
+    res.status(200).json(teachers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateTeacher = async (req, res) => {
+  try {
+    const { teacherId } = req.params;
+    const { name } = req.body;
+
+    const teacher = await Teacher.findByPk(teacherId);
+
+    if (teacher) {
+      await teacher.update({ name });
+
+      return res.status(200).json(teacher);
+    } else {
+      return res.status(404).json({ message: "Professor não encontrado." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteTeacher = async (req, res) => {
+  try {
+    const { teacherId } = req.params;
+
+    const teacher = await Teacher.findByPk(teacherId);
+
+    if (teacher) {
+      await teacher.destroy();
+
+      return res.status(200).json({
+        message: "Professor deletado com sucesso.",
+      });
+    } else {
+      return res.status(404).json({ message: "Professor não encontrado." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export { createTeacher, readTeacher, updateTeacher, deleteTeacher };
